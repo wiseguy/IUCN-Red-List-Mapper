c7bda18cb20c8061f5292a1d4f22556de0af02f0
andrewcottam <a.cottam@gmail.com>
2011-04-28 08:41:53 +0100

    Constants.js untracked

--- js/constants.js
+++ js/constants.js
@@ -1,2 +1,1 @@
-var token = "5QWY9_apTW2wMDnW_1qS0X_xzwdNkQhrmiMPTPEZj7P2GekRC-sPm69KMxQZT9sZ_Kx6cBPzUzo3QKi_bZrLuftliv3r0aBOqc2jJFeGmKM."; //for http://46.51.139.201/IUCN
-//var token = "5QWY9_apTW2wMDnW_1qS0ac44lXSgs5tLRg3Ooi2xHRcSKcyp4Z2n2t3d0JBPUtw"; //for http://
+var token = "5QWY9_apTW2wMDnW_1qS0ac44lXSgs5tLRg3Ooi2xHRcSKcyp4Z2n2t3d0JBPUtw"; //for http://
